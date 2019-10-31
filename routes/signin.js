6c71a5a3f93d040b0bef39b0d0e241d5a7dc1865
iamsaikumar <saikumarreddyatluri@gmail.com>
2019-10-31 16:52:52 +0530

    beta13

--- routes/signin.js
+++ routes/signin.js
@@ -36,8 +36,10 @@ try {
     if(user==null)res.status(404).send("user data not found").end();
     console.log(user);
     user=user.toJSON();
-        if(!(user.hasOwnProperty(fuid))){
-        var response= await Model.findOneAndUpdate({"uid":req.query.uid},{$set:{fuid: req.query.fuid,email: req.query.email,photourl:req.query.photourl}},{new:true,useFindAndModify:true});
+    console.log(user.fuid);   
+        if(user.fuid==undefined){
+        console.log("cememe");
+        var response= await Model.findOneAndUpdate({"uid":req.query.uid},{$set:{fuid: req.query.fuid,email: req.query.email},$setOnInsert:{photourl:req.query.photourl}},{new:true,upsert:true,useFindAndModify:true});
         res.status(200).send(response).end();
         }
         else{
