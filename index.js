2a30b1a4ef6192ec675fde30ba1efa237f9f3b0c
iamsaikumar <saikumarreddyatluri@gmail.com>
2020-01-07 12:03:24 +0530

    skf

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ app.use("/fee", fee);
 app.use('/college', college);
 // app.use("/queries",querries);
 mongoose.connect('mongodb+srv://couboidsclub:audibenz@cluster0-ax1bc.mongodb.net/test?retryWrites=true&w=majority', {
-    useNewUrlParser: true
+    useNewUrlParser: true,
 }, (err, client) => {
     if (!err) {
-        app.listen("4000", () => {
+        app.listen("3002", () => {
             console.log("server listening");
         });
     } else {
